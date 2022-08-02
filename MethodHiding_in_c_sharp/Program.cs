63a31c9a9583aeb1bae78f293ef91a64fd494e81
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-08-02 09:04:21 +0545

    Add project files.

---
+++ MethodHiding_in_c_sharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+/*
+1)Method Hiding can be achieve using "new keyword".
+2)When the method is been declared with the "new" keyword then it will  automatically hide the 
+    existing method of a parent class(base class)
+*/
+namespace MethodHiding_in_c_sharp
+{
+
+
+  class Class1{
+    public void display()
+    {
+        Console.WriteLine("Super class display method");
+        }
+    }
+
+    class Class2 : Class1 {
+        public new void display()
+        {
+            Console.WriteLine("sub class display method");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 two = new Class2();
+            two.display();
+            Console.ReadKey();
+        }
+        /*Conclusion sub class method hide the method of base class method */
+    }
+}
